9282826159f18ab5ac063aba3d198e0d67d16e77
Ay44n-ej4z <ayaan.ejaz185@gmail.com>
2021-09-24 17:08:39 +0530

    initial-commit

---
+++ albums/src/components/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: 'd49c05e6fcce4bbea12ad4fd22ed26fd',
+        ClientSecret: 'c396aacf4a624569997c89067219113b'
+    }
+}
+
+export { Credentials };
